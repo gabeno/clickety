832225fb142b4b33baf7071cde4d6e87555e340f
Gabriel Majivu <gabriel@angaza.com>
2020-05-07 22:21:34 +0300

    part01 lesson05
    Objectives:
    * Add new options to show how type validation works
      * Add `--int-option`
        * Validate as integer
        * Add output "int: {VALUE}" if value passed
      * Add `--float-option`
        * Validate as float
        * Add output "float: {VALUE}" if value passed
      * Add `--bool-option`
        * Validate as boolean
        * Add output "bool: {VALUE}" if value passed
      * Add `--choice-option`
        * Validate values are one of "A", "B", "C"
        * Add output "choice: {VALUE}" if value passed

--- lessons/part_01/cli.py
+++ lessons/part_01/cli.py
@@ -7,11 +7,19 @@ import click
 @click.argument("name", nargs=-1)
 @click.option("--greeting", "-g", default="Hello", help="The greeting to display")
 @click.option("--question/--no-question", default=False, help="Make the greeting a question")
-def cli(name, greeting, question):
+@click.option("--int-option", type=int)
+@click.option("--float-option", type=float)
+@click.option("--bool-option", type=bool)
+@click.option("--choice-option", type=click.Choice(["A", "B", "C"]))
+def cli(name, greeting, question, int_option, float_option, bool_option, choice_option):
     """Displays a greeting."""
     punctuation = "?" if question else "!"
     for n in name:
         print(f"{greeting}, {n}{punctuation}")
+    print(f"int: {int_option}")
+    print(f"float: {float_option}")
+    print(f"bool: {bool_option}")
+    print(f"choice: {choice_option}")
 
 
 if __name__ == "__main__":
