91445a8314a4cba9ca250027f2c2edfc2d4c74ad
Gabriel Majivu <gabriel@angaza.com>
2020-05-07 22:03:11 +0300

    part 01 lesson 04: add help documentation

--- lessons/part_01/cli.py
+++ lessons/part_01/cli.py
@@ -5,9 +5,10 @@ import click
 
 @click.command()
 @click.argument("name", nargs=-1)
-@click.option("--greeting", "-g", default="Hello")
-@click.option("--question/--no-question", default=False)
+@click.option("--greeting", "-g", default="Hello", help="The greeting to display")
+@click.option("--question/--no-question", default=False, help="Make the greeting a question")
 def cli(name, greeting, question):
+    """Displays a greeting."""
     punctuation = "?" if question else "!"
     for n in name:
         print(f"{greeting}, {n}{punctuation}")
