af3fb0ae0042fedab0237e8a0dc1629ae5b3be99
Gabriel Majivu <gabriel@angaza.com>
2020-05-07 23:04:35 +0300

    part02 lesson02
    Objectives:
    * Read from and write to files or stdin/stdout depending on arguments
      * Read the input source and write the contents to the output
      * Accept an input file argument, reading from stdin by default (using `-` arg)
      * Accept an output file argument, writing to stdout by default (using `-` arg)
      * Find the length of the input data and print a message to stderr

--- lessons/part_02/cli.py
+++ lessons/part_02/cli.py
@@ -3,13 +3,13 @@
 import click
 
 
-@click.option("--red", is_flag=True)
 @click.command()
-def cli(red):
-    click.echo("Printing...", err=True)
-
-    color = "red" if red else None
-    click.secho("Hello!", fg=color)
+@click.argument("infile", type=click.File("r"), default="-")
+@click.argument("outfile", type=click.File("w"), default="-")
+def cli(infile, outfile):
+    text = infile.read()
+    click.echo(text, file=outfile, nl=False)
+    click.echo(f"Input length: {len(text)}", err=True)
 
 
 if __name__ == "__main__":
